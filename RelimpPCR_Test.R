a7c0169ceb97e78e0cfb01cbf31ec5496ba60099
Michael Hernandez <hernandez@uchicago.edu>
2017-07-10 18:50:34 -0500

    Attempted fix for unit test on travisci

--- RelimpPCR_Test.R
+++ RelimpPCR_Test.R
@@ -11,8 +11,8 @@ test.RelimpPCR <- function() {
   slopesSet<-runif(500,1,3)
   Y<-sapply(2:500,function(z) 1+X[,1:z]%*%slopesSet[1:z]+Epsilon)
   
-  checkEquals(RelimpPCR(Y[,250],X[,1:250])$relimp_r2[1],0.008512093)
-  checkEquals(RelimpPCR(Y[,250],X[,1:250])$relimp_pca_r2[1],0.08319307)
-  checkEquals(RelimpPCR(Y[,250],X[,1:250])$pca_r2[1],0.0003308157)
-  checkEquals(RelimpPCR(Y[,250],X[,1:250])$original_r2[1],0.003703125)
+  checkEquals(RelimpPCR(Y[,250],X[,1:250])$relimp_r2[1],0.008512093,multicore=F)
+  checkEquals(RelimpPCR(Y[,250],X[,1:250])$relimp_pca_r2[1],0.08319307,multicore=F)
+  checkEquals(RelimpPCR(Y[,250],X[,1:250])$pca_r2[1],0.0003308157,multicore=F)
+  checkEquals(RelimpPCR(Y[,250],X[,1:250])$original_r2[1],0.003703125,multicore=F)
 }
