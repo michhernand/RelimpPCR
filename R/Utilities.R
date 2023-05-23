569173669801faff3a09b21a3da4d2887ae0a569
Michael Hernandez <mhernan88@gmail.com>
2023-05-23 16:25:00 -0500

    added docs

--- R/Utilities.R
+++ R/Utilities.R
@@ -1,21 +1,43 @@
-train_test_split <- function(X, Y, validation_split) {
+#' A Train/Test Split Function
+#'
+#' This function splits the data into training and testing sets.
+#'
+#' @param X (data frame): A data frame of predictors.
+#' @param Y (list/vector): A list or vector of responses.
+#' @param validation_split (numeric): A number between 0 and 1
+#' that represents the proportion of the data to be used for testing.
+#' @return out (list): A list containing all of the below components...
+#' @return $train_x (data frame): A data frame of predictors for the
+#' training set.
+#' @return $train_y (list/vector): A list or vector of responses for
+#' the training set.
+#' @return $test_x (data frame): A data frame of predictors for the testing set.
+#' @return $test_y (list/vector): A list or vector of responses for
+#' the testing set.
+#' \donttest{
+#' # Below performs a 50/50 train/test split
+#' result <- train_test_split(X = mtcars[,2:6], Y = mtcars[,1],
+#' validation_split = 0.5)
+#' @export
+#' }
+train_test_split <- function(x, y, validation_split) {
   stopifnot(validation_split > 0)
   stopifnot(validation_split <= 1)
   if (validation_split == 1) {
-    train_y <- X
-    test_x <- X
-    train_y <- Y
-    test_y <- Y
+    train_y <- x
+    test_x <- x
+    train_y <- y
+    test_y <- y
   } else {
     logger::log_info("performing train/test split")
     ix <- sample(
-        x = 1:dim(X)[1],
-        size = round(dim(X)[1] * validation_split, 0)
+        x = seq_len(dim(x)[1]),
+        size = round(dim(x)[1] * validation_split, 0)
     )
-    train_x <- X[ix,]
-    test_x <- X[-ix,]
-    train_y <- Y[ix]
-    test_y <- Y[-ix]
+    train_x <- x[ix,]
+    test_x <- x[-ix,]
+    train_y <- y[ix]
+    test_y <- y[-ix]
   }
 
   out <- list()
