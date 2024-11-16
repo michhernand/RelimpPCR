ae47fce47061d67e21118c3fad07b56f2115e88f
michael <mhernan88@gmail.com>
2024-11-15 23:36:38 -0600

    added option for desc

--- src/sort.cpp
+++ src/sort.cpp
@@ -4,12 +4,17 @@
 /**
 * @brief Produces the indices of the sorted vec.
 * @param arr The arma::vec to sort.
+* @param desc Sort in descending order.
 * @return A arma::uvec containing the sorted indices.
 */
-arma::uvec argsort_array(const arma::dvec& arr) {
+arma::uvec argsort_array(const arma::dvec& arr, bool desc = false) {
     arma::uvec indices = arma::regspace<arma::uvec>(0, arr.n_elem - 1);
     std::sort(indices.begin(), indices.end(), [&arr](arma::uword a, arma::uword b) {
+      if (desc == true) {
+        return arr[a] > arr[b];
+      } else {
         return arr[a] < arr[b];
+      }
     }
     return indices;
 }
